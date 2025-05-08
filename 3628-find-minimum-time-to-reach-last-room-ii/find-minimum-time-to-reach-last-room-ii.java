c97d42998bcc39f7872bdf80453ef65a542ea409
RAJAN PANDEY <111371095+rajans01github@users.noreply.github.com>
2025-05-09 01:19:43 +0530

    Time: 25 ms (99.60%) | Memory: 128.1 MB (8.73%) - LeetSync

--- 3628-find-minimum-time-to-reach-last-room-ii/find-minimum-time-to-reach-last-room-ii.java
+++ 3628-find-minimum-time-to-reach-last-room-ii/find-minimum-time-to-reach-last-room-ii.java
@@ -1,46 +1,110 @@
-import java.util.*;
+/* Copyright (c) 2024 by https://leetcode.com/brinuke/. All rights reserved. */
+class Solution {
+	private static class Room implements Comparable<Room> {
+		final int openTime;
+		final boolean longStay;
+		Room[] adjacent;
+		Room next;
 
-public class Solution {
-    public int minTimeToReach(int[][] moveTime) {
-        int n = moveTime.length;
-        int m = moveTime[0].length;
+		Room() {
+			openTime = Integer.MAX_VALUE;
+			longStay = true;
+		}
 
-        int[][] bestTime = new int[n][m];
-        for (int[] row : bestTime) {
-            Arrays.fill(row, Integer.MAX_VALUE);
-        }
+		Room(int openTime, boolean longStay) {
+			this.openTime = openTime;
+			this.longStay = longStay;
+			next = this; // indicates that this Room hasn't been approached yet
+		}
 
-        int[][] directions = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
-        PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(a -> a[0]));
-        pq.offer(new int[]{0, 0, 0, 0}); // time, i, j, nextMoveTime
+		@Override
+		public int compareTo(Room other) {
+			return openTime - other.openTime;
+		}
+	}
 
-        while (!pq.isEmpty()) {
-            int[] curr = pq.poll();
-            int time = curr[0], i = curr[1], j = curr[2], nextMoveTime = curr[3];
+	private static final Room DUMMY_ROOM = new Room();
 
-            if (time >= bestTime[i][j]) continue;
-            bestTime[i][j] = time;
+	private static Room initRooms(int[][] moveTime) {
+		int n = moveTime.length;
+		int m = moveTime[0].length;
+		Room[][] rooms = new Room[n][m];
+		for (int i = 0; i < n; i++) {
+			int[] mtRow = moveTime[i];
+			Room[] rRow = rooms[i];
+			for (int j = 0; j < m; j++)
+				rRow[j] = new Room(mtRow[j], ((i + j) & 1) == 0);
+		}
+		Room[] dummyRow = new Room[m];
+		Arrays.fill(dummyRow, DUMMY_ROOM);
+		Room[] prevRow = dummyRow;
+		Room[] curRow = rooms[0];
+		n--;
+		m--;
+		for (int i = 0; i <= n; i++) {
+			Room[] nextRow = i < n ? rooms[i + 1] : dummyRow;
+			Room prev = DUMMY_ROOM;
+			Room cur = curRow[0];
+			for (int j = 0; j <= m; j++) {
+				Room next = j < m ? curRow[j + 1] : DUMMY_ROOM;
+				cur.adjacent = new Room[] { prev, prevRow[j], next, nextRow[j] };
+				prev = cur;
+				cur = next;
+			}
+			prevRow = curRow;
+			curRow = nextRow;
+		}
+		Room start = rooms[0][0];
+		start.next = rooms[n][m]; // finish
+		return start;
+	}
 
-            if (i == n - 1 && j == m - 1) return time;
-
-            for (int[] d : directions) {
-                int x = i + d[0], y = j + d[1];
-                if (x >= 0 && x < n && y >= 0 && y < m) {
-                    int wait = moveTime[x][y];
-                    int futureMove = nextMoveTime == 1 ? 2 : 1;
-                    int nextTime = wait > time ? wait + futureMove : time + futureMove;
-
-                    if (i == 0 && j == 0 && wait <= time) {
-                        nextTime = wait + futureMove;
-                    }
-
-                    if (nextTime < bestTime[x][y]) {
-                        pq.offer(new int[]{nextTime, x, y, futureMove});
-                    }
-                }
-            }
-        }
-
-        return -1; // Should never reach
-    }
+	public static int minTimeToReach(int[][] moveTime) {
+		Room start = initRooms(moveTime);
+		Room finish = start.next;
+		Queue<Room> waitingToEnter = new PriorityQueue<>();
+		waitingToEnter.add(DUMMY_ROOM); // guard Room to prevent NPE
+		start.next = null;
+		Room exitingShortHead = start;
+		Room exitingLongHead = null;
+		int currentTime = 0;
+		while (true) {
+			Room exitingLongHeadNew = null;
+			while (exitingShortHead != null) {
+				for (Room adj : exitingShortHead.adjacent)
+					if (adj.next == adj) {
+						if (adj == finish)
+							return Math.max(currentTime, finish.openTime) + (finish.longStay ? 2 : 1);
+						if (adj.openTime <= currentTime) {
+							if (adj.longStay) {
+								adj.next = exitingLongHeadNew;
+								exitingLongHeadNew = adj;
+							} else {
+								adj.next = exitingLongHead;
+								exitingLongHead = adj;
+							}
+						} else {
+							adj.next = null;
+							waitingToEnter.offer(adj);
+						}
+					}
+				exitingShortHead = exitingShortHead.next;
+			}
+			exitingShortHead = exitingLongHead;
+			exitingLongHead = exitingLongHeadNew;
+			int queueTime;
+			while ((queueTime = waitingToEnter.peek().openTime) <= currentTime) {
+				Room entering = waitingToEnter.poll();
+				if (entering.longStay) {
+					entering.next = exitingLongHead;
+					exitingLongHead = entering;
+				} else {
+					entering.next = exitingShortHead;
+					exitingShortHead = entering;
+				}
+			}
+			if (++currentTime < queueTime && exitingShortHead == null && exitingLongHead == null)
+				currentTime = queueTime;
+		}
+	}
 }
