e7003daff6ea80f512b57f1289107191f016713b
RAJAN PANDEY <111371095+rajans01github@users.noreply.github.com>
2025-07-23 12:41:47 +0530

    Time: 6 ms (85.48%) | Memory: 43.1 MB (59.50%) - LeetSync

---
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String reverseWords(String s) {
+         StringBuilder sb = new StringBuilder();
+
+        String[] words = s.trim().split("\\s+");
+
+        for(int i = words.length -1; i >= 0; i--){
+            sb.append(words[i]);
+            if( i != 0)sb.append(" ");
+        }
+        return sb.toString();
+    }
+}
