90c78fb198bb10ff2a8c79dcf4e8826af6006eeb
RAJAN PANDEY <111371095+rajans01github@users.noreply.github.com>
2025-08-04 16:04:54 +0530

    Time: 1 ms (99.85%) | Memory: 46.3 MB (35.34%) - LeetSync

--- 136-single-number/single-number.java
+++ 136-single-number/single-number.java
@@ -1,11 +1,10 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int result = 0 ;
+      int result = 0;
 
-        for(int num : nums){
-            result ^= num;
-        }
-        return result;
-        
+      for(int num : nums){
+        result ^= num;
+      }
+      return result;
     }
 }
