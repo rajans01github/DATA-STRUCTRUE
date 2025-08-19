8a56445237b734273d9d850c2a5c40015a019129
RAJAN PANDEY <111371095+rajans01github@users.noreply.github.com>
2025-08-19 15:17:28 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (67.18%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
@@ -1,9 +1,32 @@
 class Solution {
     public boolean search(int[] nums, int target) {
-        
-        for(int num : nums){
-            if(num == target){
-                return true;
+        int low = 0, high = nums.length - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] == target) return true;
+
+            // अगर duplicates हैं
+            if (nums[low] == nums[mid] && nums[mid] == nums[high]) {
+                low++;
+                high--;
+            }
+            // Left side sorted
+            else if (nums[low] <= nums[mid]) {
+                if (target >= nums[low] && target < nums[mid]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            }
+            // Right side sorted
+            else {
+                if (target > nums[mid] && target <= nums[high]) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
             }
         }
         return false;
