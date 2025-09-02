c60d778aa6f26b322e626ea6e902dcbf0bb7631c
RAJAN PANDEY <111371095+rajans01github@users.noreply.github.com>
2025-09-02 21:27:58 +0530

    Time: 6 ms (73.13%) | Memory: 44.5 MB (79.10%) - LeetSync

---
+++ 3278-find-the-number-of-ways-to-place-people-i/find-the-number-of-ways-to-place-people-i.java
@@ -0,0 +1,22 @@
+class Solution {
+   public int numberOfPairs(int[][] points) {
+    Arrays.sort(points, (a, b) -> a[0] == b[0] ? 
+        b[1] - a[1] : a[0] - b[0]);
+    int cnt = 0;
+    for (int i = 0; i < points.length; i++) {
+        int x0 = points[i][0], y0 = points[i][1];
+        int bot = Integer.MIN_VALUE, top = y0;
+        for (int j = i + 1; j < points.length; j++) {
+            int x1 = points[j][0], y1 = points[j][1];
+            if (y1 <= top && y1 > bot) {
+                // found a valid rectangular.
+                cnt++;
+                bot = y1;
+                // to avoid points on the same y value;
+                if (y1 == top) top--;
+            }
+        }
+    }
+    return cnt;
+}
+}
