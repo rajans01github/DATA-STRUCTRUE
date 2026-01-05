54a1fd20f6cb15f2a9145971487c1166333d82d7
RAJAN PANDEY <111371095+rajans01github@users.noreply.github.com>
2026-01-05 10:17:27 +0530

    Time: 4 ms (99.69%) | Memory: 48.1 MB (5.65%) - LeetSync

--- 680-valid-palindrome-ii/valid-palindrome-ii.java
+++ 680-valid-palindrome-ii/valid-palindrome-ii.java
@@ -14,8 +14,8 @@ class Solution {
 
     public boolean validPalindrome(String s) {
 
-        int left = 0;
-        int right = s.length() - 1;
+        int left = 0,
+        right = s.length() - 1;
 
         while (left < right) {
 
