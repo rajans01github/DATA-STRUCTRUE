8c66ec32a9c5525c958ce98d921df4f6cabace28
RAJAN PANDEY <111371095+rajans01github@users.noreply.github.com>
2026-01-06 09:12:29 +0530

    Time: 4 ms (99.68%) | Memory: 48 MB (5.69%) - LeetSync

--- 680-valid-palindrome-ii/valid-palindrome-ii.java
+++ 680-valid-palindrome-ii/valid-palindrome-ii.java
@@ -1,33 +1,30 @@
 class Solution {
-
-    // helper function: normal palindrome check
-    public boolean Palin(String s, int i, int j) {
-        while (i < j) {
-            if (s.charAt(i) != s.charAt(j)) {
+    public boolean palin(int left , int  right, String s ){
+        while(left < right){
+            if(s.charAt(left) != s.charAt(right)){
                 return false;
             }
-            i++;
-            j--;
+            left++;
+            right--;
         }
         return true;
     }
-
     public boolean validPalindrome(String s) {
 
-        int left = 0,
-        right = s.length() - 1;
+        int left = 0;
+        int right = s.length() - 1;
 
-        while (left < right) {
+        while(left < right){
 
-            if (s.charAt(left) == s.charAt(right)) {
+            if(s.charAt(left) == s.charAt(right)){
                 left++;
                 right--;
-            } else {
-                // ek delete allowed
-                return Palin(s, left + 1, right) ||
-                       Palin(s, left, right - 1);
+            }
+            else{
+                return palin(left + 1, right , s) || palin(left , right-1,s);
             }
         }
         return true;
+        
     }
 }
